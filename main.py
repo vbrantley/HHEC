4d8430b560ea2bae76944cbcc25c55db6d6cdc52
Van Brantley <van.brantley@gmail.com>
2020-03-30 15:57:42 -0400

    populating website skeleton

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import json
+from sys import argv
+from flask import Flask, request, make_response, redirect, url_for, session, jsonify
+from flask import render_template
+# from flask_heroku import Heroku
+
+app = Flask(__name__, template_folder='templates')
+
+#-------------------------------------------------------------------------------
+
+@app.route('/')
+def index():
+
+    html = render_template('index.html')
+    response = make_response(html)
+    return(response)
+
+#-------------------------------------------------------------------------------
+
+@app.route('/program')
+def program():
+
+    html = render_template('program.html')
+    response = make_response(html)
+    return(response)
+
+#-------------------------------------------------------------------------------
+
+@app.route('/FAQ')
+def FAQ():
+
+    html = render_template('faq.html')
+    response = make_response(html)
+    return(response)
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print('Usage: ' + argv[0] + ' port')
+        exit(1)
+    app.run(host='0.0.0.0', port=int(argv[1]), debug=True)
