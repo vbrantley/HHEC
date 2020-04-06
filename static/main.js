e4c0269a1c377c0d7c30ed372df1ed55f7decbb0
Van Brantley <van.brantley@gmail.com>
2020-04-06 16:35:19 -0400

    created js file, and added functionality to FAQ page

---
+++ static/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+   $('#question1').click(function() {
+      $('#answer1').toggle("slide");
+   });
+
+   $('#question2').click(function() {
+      $('#answer2').toggle("slide");
+   });
+
+   $('#question3').click(function() {
+      $('#answer3').toggle("slide");
+   });
+
+   $('#question4').click(function() {
+      $('#answer4').toggle("slide");
+   });
+
+   $('#question5').click(function() {
+      $('#answer5').toggle("slide");
+   });
+
+   $('#question6').click(function() {
+      $('#answer6').toggle("slide");
+   });
+
+   $('#question7').click(function() {
+      $('#answer7').toggle("slide");
+   });
+
+   $('#question8').click(function() {
+      $('#answer8').toggle("slide");
+   });
+
+   $('#question9').click(function() {
+      $('#answer9').toggle("slide");
+   });
+
+});
+
+// Functions to show FAQ answers onClick
